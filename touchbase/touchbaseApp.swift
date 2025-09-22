be96c4d4fe3dd1d84cc01c78dd5a695c48dd574f
Jassim <bdjoejassim@gmail.com>
2025-09-21 21:19:52 -0400

    Initial Commit

---
+++ touchbase/touchbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchbaseApp.swift
+//  touchbase
+//
+//  Created by Jassim Ahmed on 2025-09-21.
+//
+
+import SwiftUI
+
+@main
+struct touchbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
